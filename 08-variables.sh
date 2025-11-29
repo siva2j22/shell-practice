d77180e7dddc603d4b3d7d55b0b02bddd5686096
siva2j22 <siva2j22@gmail.com>
2025-11-29 15:43:23 +0530

    shellscript code

--- 08-variables.sh
+++ 08-variables.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "time stamp executed :: $DATE"
+#echo "time stamp executed :: $DATE"
+
+#!/bin/bash
+
+#DATE=$(date)
+
+START_TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script executed in: $TOTAL_TIME Seconds"
