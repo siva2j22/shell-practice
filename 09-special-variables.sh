404d65d4269b1567fb0efc53535d357501038e0f
siva2j22 <siva2j22@gmail.com>
2025-11-29 16:02:44 +0530

    shellscript code

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -6,3 +6,7 @@ echo "Script name:: $0"
 echo "current directory:: $PWD"
 echo "how is running this:: $USER"
 echo "how directory of user:: $HOME"
+
+echo "pid of the script :: $$"
+sleep 50 &
+echo "PID of the last command in background is :: $!"
