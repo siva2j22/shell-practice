196937bd0050e64d9858f72c296f7ef6e87466a6
siva2j22 <siva2j22@gmail.com>
2025-11-29 16:34:05 +0530

    shellscript code

--- 10-data-types.sh
+++ 10-data-types.sh
@@ -7,6 +7,9 @@ name=siva
 sum=$(($value1+$value2+$name))
 echo "value are :: ${sum} "
 
+# size is 3 and index is 2
 leaders=("siva" "charan" "prasad")
 
 echo "array output are:  ${leaders[@]}" 
+echo "array output are:  ${leaders[1]}" 
+echo "array output are:  ${leaders[10]}" 
