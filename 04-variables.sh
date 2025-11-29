9bac087cd2a272951e39766d619d977cf06a2d69
siva2j22 <siva2j22@gmail.com>
2025-11-29 12:43:44 +0530

    shell commands

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=siva
+PERSON2=NAG
+
+
+#with refarance
+echo "welcome to new world $PERSON1"
+echo "welcome to new world $PERSON2"
+
+# without referance
+echo "welcome to new world SIVA"
+echo "welcome to new world NAG"
