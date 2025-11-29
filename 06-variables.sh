987fba2e47848deb002f69be8a3ef6b147646419
siva2j22 <siva2j22@gmail.com>
2025-11-29 15:13:20 +0530

    shellscript code

---
+++ 06-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please enter your PIN number"
+read -s PIN_NUMBER # here PIN_NUMBER is variable
+# if we keep -s means it will be disable it woun't apear in console
+
+echo "PIN entered is: $PIN_NUMBER"
