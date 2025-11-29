9141d9069b0bc82ba94e0df5e8f884afebb07b85
siva2j22 <siva2j22@gmail.com>
2025-11-29 15:58:53 +0530

    shellscript code

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -2,3 +2,7 @@
 
 echo "enter the values ::  $@"
 echo "enter the values ::  $*"
+echo "Script name:: $0"
+echo "current directory:: $PWD"
+echo "how is running this:: $USER"
+echo "how directory of user:: $HOME"
