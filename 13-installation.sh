30654fc99cd44bc5c55b514543104b78b8c529fa
siva2j22 <siva2j22@gmail.com>
2025-11-30 12:14:13 +0530

    practice shell commands

--- 13-installation.sh
+++ 13-installation.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-USEDID=$(id -u)
+USERID=$(id -u)
 
 if [ USERID -ne 0 ]; then
     echo "Error:: please run with root previlages"
+    exit 25
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "error:: installtion is fail"
+    exit 52
 else
     echo "success: installed the software"
 fi
